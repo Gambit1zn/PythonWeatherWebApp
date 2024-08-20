80fd5f3781a24b74462ded203dbc588ba6518d24
Ethan Ankadu <ethan.ankadu@gamesglobal.com>
2024-08-20 19:17:31 +0200

    Added backend code that fetches real weather data

---
+++ weather-web-app/backend.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = "87a93f2ae35c5e255fce913d72f27e74"
+
+
+def get_data(place, forcast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forcast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forcast_days=3, kind="Temperature"))
